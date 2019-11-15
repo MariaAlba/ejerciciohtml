afbde4541566f33788fe3e65de8516d604334a0a
Maria <Curso@IPARTEKAULA.COM>
2019-11-15 13:19:47 +0100

    posicionamiento

--- pruebatecnica/js/game.js
+++ pruebatecnica/js/game.js
@@ -26,24 +26,106 @@ var paddleX = (canvas.width - paddleWidth) / 2;
 //para controlar la pala
 var rightPressed = false;
 var leftPressed = false;
+
 document.addEventListener("keydown", keyDownHandler, false);
 document.addEventListener("keyup", keyUpHandler, false);
+document.addEventListener("mousemove", mouseMoveHandler, false);
+
+function mouseMoveHandler(e) {
+    var relativeX = e.clientX - canvas.offsetLeft;
+    if (relativeX > 0 && relativeX < canvas.width) {
+        paddleX = relativeX - paddleWidth / 2;
+    }
+}
+
+//variables ladrillos
+var brickRowCount = 3;
+var brickColumnCount = 5;
+var brickWidth = 75;
+var brickHeight = 20;
+var brickPadding = 10;
+var brickOffsetTop = 30;
+var brickOffsetLeft = 30;
+
+var bricks = [];
+for (c = 0; c < brickColumnCount; c++) {
+    bricks[c] = [];
+    for (r = 0; r < brickRowCount; r++) {
+        bricks[c][r] = { x: 0, y: 0, status: 1 };
+    }
+}
+
+//contador
+var score = 0;
+
+//vidas
+var lives = 3;
 
 function keyDownHandler(e) {
-    if(e.keyCode == 39) {
+    if (e.keyCode == 39) {
         rightPressed = true;
     }
-    else if(e.keyCode == 37) {
+    else if (e.keyCode == 37) {
         leftPressed = true;
     }
 }
 
 function keyUpHandler(e) {
-    if(e.keyCode == 39) {
+    if (e.keyCode == 39) {
         rightPressed = false;
     }
-    else if(e.keyCode == 37) {
+    else if (e.keyCode == 37) {
         leftPressed = false;
+    }
+}
+
+function drawScore() {
+    ctx.font = "16px Arial";
+    ctx.fillStyle = "#0095DD";
+    ctx.fillText("Score: " + score, 8, 20);
+}
+
+function drawLives() {
+    ctx.font = "16px Arial";
+    ctx.fillStyle = "#0095DD";
+    ctx.fillText("Lives: "+lives, canvas.width-65, 20);
+}
+
+function collisionDetection() {
+    for (c = 0; c < brickColumnCount; c++) {
+        for (r = 0; r < brickRowCount; r++) {
+            var b = bricks[c][r];
+            if (b.status == 1) {
+                if (x > b.x && x < b.x + brickWidth && y > b.y && y < b.y + brickHeight) {
+                    dy = -dy;
+                    b.status = 0;
+                    score++;
+                    if (score == brickRowCount * brickColumnCount) {
+                        alert("YOU WIN, CONGRATULATIONS!");
+                        document.location.reload();
+                    }
+                }
+            }
+        }
+    }
+}
+
+//dibujar ladrillos
+function drawBricks() {
+    for (c = 0; c < brickColumnCount; c++) {
+        for (r = 0; r < brickRowCount; r++) {
+            if (bricks[c][r].status == 1) {
+                var brickX = (c * (brickWidth + brickPadding)) + brickOffsetLeft;
+                var brickY = (r * (brickHeight + brickPadding)) + brickOffsetTop;
+                bricks[c][r].x = brickX;
+                bricks[c][r].y = brickY;
+                ctx.beginPath();
+                ctx.rect(brickX, brickY, brickWidth, brickHeight);
+                ctx.fillStyle = "#0095DD";
+                ctx.fill();
+                ctx.closePath();
+            }
+        }
     }
 }
 
@@ -57,7 +139,7 @@ function drawBall(color) {
     ctx.fillStyle = color;
     ctx.fill();
     ctx.closePath();
-   
+
 }
 
 function drawPaddle() {
@@ -72,10 +154,18 @@ function draw() {
     //borrar rectangulo
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    //dibujar pala
-    drawPaddle();
+    //dibujar ladrillos
+    drawBricks();
     //dibujar
     drawBall();
+    //dibujar pala
+    drawPaddle();
+    //contador
+    drawScore();
+    //vidas
+    drawLives();
+    //
+    collisionDetection();
 
 
     //borde izquierda y derecha
@@ -86,38 +176,51 @@ function draw() {
     }
 
     //borde inferior y superior
-   /*  if (y + dy > canvas.height - ballRadius || y + dy < ballRadius) {
-        dy = -dy;
-        drawBall(randomColor());
-    } */
+    /*  if (y + dy > canvas.height - ballRadius || y + dy < ballRadius) {
+         dy = -dy;
+         drawBall(randomColor());
+     } */
     //modificacion para que no pueda tocar el borde inferior
-    if(y + dy < ballRadius) {
+    if (y + dy < ballRadius) {
         dy = -dy;
-    } else if(y + dy > canvas.height-ballRadius) {
-        if(x > paddleX && x < paddleX + paddleWidth) {
+    } else if (y + dy > canvas.height - ballRadius) {
+        if (x > paddleX && x < paddleX + paddleWidth) {
             dy = -dy;
+
         }
         else {
-            alert("GAME OVER");
-            //document.location.reload();
+            lives--;
+            if (!lives) {
+                alert("GAME OVER");
+                document.location.reload();
+            }
+            else {
+                x = canvas.width / 2;
+                y = canvas.height - 30;
+                dx = 2;
+                dy = -2;
+                paddleX = (canvas.width - paddleWidth) / 2;
+            }
         }
     }
-    
+
     //mover la pala
-    if(rightPressed && paddleX < canvas.width-paddleWidth) {
+    if (rightPressed && paddleX < canvas.width - paddleWidth) {
         paddleX += 7;
     }
-    else if(leftPressed && paddleX > 0) {
+    else if (leftPressed && paddleX > 0) {
         paddleX -= 7;
     }
-    
+
     //actualizar posicion
     x += dx;
     y += dy;
+
+    requestAnimationFrame(draw);
 }
 
 
-setInterval(draw, 10);
+draw();
 
 
 function randomColor() {
